3bedf6c7508a2469d5db4827f2a7a6bba15f723a
greedymelon <74475072+greedymelon@users.noreply.github.com>
2023-05-09 18:24:37 +0200

    Update RequestHandler.cpp

--- RequestHandler.cpp
+++ RequestHandler.cpp
@@ -1,7 +1,15 @@
 #include "RequestHandler.hpp"
+#include <cstring>
+#include <iostream>
 
+void RequestHandler::set_method(std::string first_line)
+{
+    std::string address = first_line.substr(first_line.find_first_of("/"), first_line.find_last_of(" ") - first_line.find_first_of("/"));
+    address = address.substr(0, address.find_first_of(" "));
+    this->_page = address.c_str();
+} 
 
-RequestHandler::RequestHandler(Configuration &data, Request *client):_data(data), _client(client), _response(new Response)
+RequestHandler::RequestHandler(Configuration &data, Request &request):_conf(data), _clientRequest(request), _response(new Response)
 {
     set_response();
 }
@@ -15,7 +23,10 @@ Response *RequestHandler::get_response() const
 
 void RequestHandler::set_response(void)
 {
-    set_method();
+    std::string first_line;
+    std::getline(_clientRequest.get_fd(), first_line);
+    set_method(first_line);
+    set_page(first_line);
     switch(this->_method)
     {
         case GET:
@@ -30,4 +41,33 @@ void RequestHandler::set_response(void)
         default:
             set_error();
     }
+}
+void RequestHandler::set_method(std::string first_line)
+{
+    if (first_line.find_first_of("HTTP/1.1") == std::string::npos)
+    {
+        _method = WRONG_PROTOCOL;
+        return ;
+    }
+    if (first_line.find_first_of("GET ") == 0)
+        _method = GET;
+    else if (first_line.find_first_of("POST ") == 0)
+        _method = POST;
+    else if (first_line.find_first_of("DELETE ") == 0)
+        _method = DELETE;
+    else
+        _method = METHOD_NOT_ALLOW;
+    
+}
+void RequestHandler::set_get(void)
+{
+    std::ifstream ifs;
+    std::ofstream ofs;
+
+    ifs.open(_page);
+    ofs.open("fakeRespose");
+    ofs << "HTTP/1.1 200 OK" << std::endl;
+
+
+
 }
